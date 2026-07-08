185773a93f4337f4a3b96e572dae7cb366ec208d
XA Yu <XAYu2026@outlook.com>
2026-07-08 08:45:45 +0800

    Delete cal_ETASI_Bounds.m

--- cal_ETASI_Bounds.m
+++
@@ -1,8 +0,0 @@
-function obj=cal_ETASI_Bounds(z, time, mag0, LB, UB, smartSample)
-% Bounded objective for fminsearch fallback
-if any(z < 0) || any(z > 1) || any(~isfinite(z))
-    obj = 1e20 + 1e10*sum((min(z,0)).^2 + (max(z-1,0)).^2);
-    return;
-end
-obj = Cost_ETASI(z, time, mag0, LB, UB, smartSample);
-end
