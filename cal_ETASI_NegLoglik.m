f07c8ca6550ff4b528be30b9e111e9249649997b
XA Yu <XAYu2026@outlook.com>
2026-07-08 08:46:17 +0800

    Delete cal_ETASI_NegLoglik.m

--- cal_ETASI_NegLoglik.m
+++
@@ -1,9 +0,0 @@
-function negLogL = cal_ETASI_NegLoglik(z, time, mag0, LB, UB, smartSample)
-% Negative log-likelihood of the ETASI model
-theta = cal_UnitToParams(z, LB, UB);
-[negLogL, badflag] = cal_ETASI_NegLogLik_Inner(theta, time, mag0, smartSample);
-if badflag || ~isfinite(negLogL)
-    negLogL = 1e20;
-end
-
-end
